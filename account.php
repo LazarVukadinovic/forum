b83532e080c2d2d01af03e4a5691f0e524874a8d
LAKAZET <lazolakivukadinovic1001@gmail.com>
2022-11-11 18:48:46 +0100

    display own posts - account

--- account.php
+++ account.php
@@ -37,66 +37,120 @@
                 color: #fff !important;
                 
             }
+            .objava{
+                width: 100%;
+                background-color: #ebf2fa;
+                border: 2px solid black;
+                border-radius: 5px;
+                padding: 15px 20px 10px 20px;
+            }
+            .objava h5{
+                display:inline-block;
+            }
+            .objava p{
+                display:inline;
+            }
+            small{
+                opacity: 0.8;
+            }
+            .objava a{
+                text-decoration: none !important;
+                color: #000 !important;
+            }
+            .objava a:hover{
+                text-decoration: underline !important;
+            }
         </style>
     </head>
+    <?php 
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if (!empty($_POST["spassword"]))
+                $spassword = test_input($_POST["spassword"]);
+            if (!empty($_POST["npassword"]))
+                $npassword = test_input($_POST["npassword"]);
+            if (!empty($_POST["rpassword"]))
+                $rpassword = test_input($_POST["rpassword"]);
+        }
+        $sql = "SELECT korisnicko_ime, lozinka, ime, prezime FROM korisnik WHERE korisnicko_ime='" . $_SESSION["user"] . "'";
+        $result = $conn->query($sql);
+        $row = $result->fetch_assoc();
+
+        if(isset($spassword) && isset($npassword) && isset($rpassword))
+        if((password_verify($spassword, $row["lozinka"]) && !empty($npassword) && !empty($rpassword)))
+        {
+            $sql = "UPDATE korisnik SET lozinka = '" . password_hash($npassword, PASSWORD_BCRYPT) . "' WHERE korisnicko_ime = '" . $_SESSION["user"] . "'";
+            $result = $conn->query($sql);
+        }
+
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
     <body>
         <?php include "./elements/navbar.php";?>
         <div class="container">
             <div class="row mt-5">
-                <div class="col-md-5 mt-4">
+                <div class="col-md-3 mt-4">
                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png">
                 </div>
-                <div class="col-md-7">
-                
+                <div class="col-md-8">
                     <h2 class="mb-3">DETALJI NALOGA</h2>
-                    <p>Korisnicko ime: <span><?php echo $_SESSION["user"]?></span></p>
-                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-
-                        <div class="mb-3">
-                            <input type="password" class="form-control" name="spassword" placeholder="Stara lozinka">
+                    <div class="row">
+                        <div class="col-md-5">
+                            <p>Korisnicko ime: <span><?php echo $_SESSION["user"]?></span></p>
+                            <p>Ime: <span><?php echo $row["ime"]?></span></p>
+                            <p>Prezime: <span><?php echo $row["prezime"]?></span></p>
                         </div>
+                        <div class="col-md-7">
+                            <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
-                        <div class="mb-3">
-                            <input type="password" class="form-control" name="npassword" placeholder="Nova lozinka">
+                                <div class="mb-3">
+                                    <input type="password" class="form-control" name="spassword" placeholder="Stara lozinka">
+                                </div>
+
+                                <div class="mb-3">
+                                    <input type="password" class="form-control" name="npassword" placeholder="Nova lozinka">
+                                </div>
+                                <div class="mb-3">
+                                    <input type="password" class="form-control" name="rpassword" placeholder="Ponovite novu lozinku">
+                                </div>
+                                <div class="text-center d-inline">
+                                    <button type="submit" class="btn btn-colorc px-5 mb-4">Promeni</button>
+                                </div>
+                            </form>
+                                
                         </div>
-                        <div class="mb-3">
-                            <input type="password" class="form-control" name="rpassword" placeholder="Ponovite novu lozinku">
-                        </div>
-                        <div class="text-center d-inline">
-                            <button type="submit" class="btn btn-colorc px-5 mb-4">Promeni</button>
-                        </div>
-                    </form>
-                    <?php 
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-                            if (!empty($_POST["spassword"]))
-                                $spassword = test_input($_POST["spassword"]);
-                            if (!empty($_POST["npassword"]))
-                                $npassword = test_input($_POST["npassword"]);
-                            if (!empty($_POST["rpassword"]))
-                                $rpassword = test_input($_POST["rpassword"]);
-                        }
-                        $sql = "SELECT korisnicko_ime, lozinka FROM korisnik WHERE korisnicko_ime='" . $_SESSION["user"] . "'";
-                        $result = $conn->query($sql);
-                        $row = $result->fetch_assoc();
-
-                        if(isset($spassword) && isset($npassword) && isset($rpassword))
-                        if((password_verify($spassword, $row["lozinka"]) && !empty($npassword) && !empty($rpassword)))
-                        {
-                            $sql = "UPDATE korisnik SET lozinka = '" . password_hash($npassword, PASSWORD_BCRYPT) . "' WHERE korisnicko_ime = '" . $_SESSION["user"] . "'";
-                            $result = $conn->query($sql);
-                        }
-
-
-                        function test_input($data) {
-                            $data = trim($data);
-                            $data = stripslashes($data);
-                            $data = htmlspecialchars($data);
-                            return $data;
-                        }
-                    ?>
+                    </div>
+                    
                 </div>
             </div>
+            <?php 
+               $sql = "SELECT id, naziv_teme, opis_teme, datum_kreiranja, kreator FROM tema WHERE kreator = '" . $_SESSION["user"] . "' ORDER BY datum_kreiranja DESC";
+               $result = $conn->query($sql);
+               
+
+               if ($result->num_rows > 0) {
+                   while($row = $result->fetch_assoc()){
+                       $sql_kreator = "SELECT ime, prezime FROM korisnik WHERE korisnicko_ime = '" . $row["kreator"] . "'";
+                       $result_kreator = $conn->query($sql_kreator);
+                       $podaci = $result_kreator->fetch_assoc();
+                       echo '<div class="row">';
+                           echo '<div class="col">';
+                               echo "<div class='objava mt-2'>
+                                   <a href='./diskusija.php?id=" . $row["id"] . "'><h5>" . $row["naziv_teme"] . "</h5>
+                                   <p> ● " . $row["opis_teme"] . "</p></a>
+                                   <small style='display:block; float:right'>" . $podaci["ime"] . " " . $podaci["prezime"] . " - " . date_format(date_create($row["datum_kreiranja"]), "d.m.Y H:i") . "</small>
+                               </div>";
+                           echo '</div>';
+                       echo '</div>';
+                  }
+               } 
+            ?>
         </div>
 
 
